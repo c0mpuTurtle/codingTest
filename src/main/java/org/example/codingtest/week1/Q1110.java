d31dffeebf99775e660904a3a52499936fac102b
hyebin <shin378378@naver.com>
2025-10-06 02:05:37 +0900

    Feat : Q1110 백준 풀기

---
+++ src/main/java/org/example/codingtest/week1/Q1110.java
@@ -0,0 +1,27 @@
+package org.example.codingtest.week1;
+
+import java.io.*;
+
+public class Q1110 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int input = Integer.parseInt(br.readLine());
+        int output = input;
+
+        int cycle = 0;
+
+        do {
+            int A = output / 10;
+            int B = output % 10;
+            output = B * 10+(A+B)%10;
+            cycle++;
+        }while(input!=output);
+
+        bw.write(String.valueOf(cycle));
+
+        br.close();
+        bw.close();
+    }
+}
