63ca6eb9ea3509a35c9841f632dcdf20366e41b0
hyebin <shin378378@naver.com>
2025-10-06 02:06:11 +0900

    Feat : Q27159 백준 풀기

--- src/main/java/org/example/codingtest/week1/Q27159.java
+++ src/main/java/org/example/codingtest/week1/Q27159.java
@@ -1,11 +1,29 @@
 package org.example.codingtest.week1;
 
 import java.io.*;
+import java.util.Arrays;
 
 public class Q27159 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int count = Integer.parseInt(br.readLine());
+
+        int[] arr = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt).
+                toArray();
+
+        int sum = arr[0];
+        for (int i = 1; i < count; i++) {
+            if (arr[i - 1] + 1 != arr[i]) {
+                sum += arr[i];
+            }
+        }
+        bw.write(sum + "\n");
+
+        br.close();
+        bw.close();
     }
+    //아직 못 품
 }
