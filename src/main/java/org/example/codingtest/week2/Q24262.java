3b6e08718f2b543d414fe4e8b805f9739620dbf0
hyebin <shin378378@naver.com>
2025-10-26 21:55:30 +0900

    Feat : Q24262 week2

--- src/main/java/org/example/codingtest/week2/Q24262.java
+++ src/main/java/org/example/codingtest/week2/Q24262.java
@@ -1,5 +1,8 @@
 package org.example.codingtest.week2;
 
 public class Q24262 {
-
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(0);
+    }
 }
